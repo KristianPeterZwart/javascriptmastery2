929193466b40f9c2a26747c00f9d44bb01482b2e
Kris <kristianzwartdesign@gmail.com>
2021-04-19 16:21:49 +0200

    Constructor

---
+++ constructor.js
@@ -0,0 +1,20 @@
+// Factory Function - return 
+function createCircle(radius) {
+    return {
+        radius,
+        draw: function() {
+            console.log('draw');
+        }
+        };
+    } 
+
+const circle = createCircle(1);
+
+// Constructor Function - this
+function Circle(radius) {
+   this.radius = radius;
+   this.draw = function() {
+       console.log('draw');
+   }  
+}
+const another = new Circle(1); 
