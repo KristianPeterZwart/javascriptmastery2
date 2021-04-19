79f4820606b42b0456ad67836594d33b366b4197
Kris <kristianzwartdesign@gmail.com>
2021-04-19 16:26:41 +0200

    Create objectliteral.js

---
+++ objectliteral.js
@@ -0,0 +1,14 @@
+// Object Literal Syntax
+
+let x = {};
+
+// let x = new Object();
+
+// String Literal Syntax - cleaner or simpler
+
+new String(); // ''. "", ``
+new Boolean(); // true, false
+new Number(); // 1, 2, 3, ...
+
+// Every object has a constructor property 
+// and that references the function that was used to create that object.
