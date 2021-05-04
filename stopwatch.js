8ece3c71ec2a49f627e2c0197dd1012c68cf2f00
Kris <kristianzwartdesign@gmail.com>
2021-05-04 10:39:38 +0200

    Update stopwatch.js

--- stopwatch.js
+++ stopwatch.js
@@ -5,5 +5,12 @@ let startTime, endTime, running, duration = 0;
 this.start = function() {
 };
 
+this.stop = function() {
+};
 
+this.reset = function() {
+};
+
+Object.defineProperty(this, 'duration', {
+});
 }
