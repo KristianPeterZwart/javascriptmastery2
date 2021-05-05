9fc87eee9201f6f5b7d4bdd231b4f380d5a8b73a
Kris <kristianzwartdesign@gmail.com>
2021-05-05 14:57:37 +0200

    Update propertydescriptor.js

--- propertydescriptor.js
+++ propertydescriptor.js
@@ -1,5 +1,12 @@
 
 let person = { name: 'Kris' };
-let objectBase = Object.getPrototypeOf(person); 
-let descriptor = Object.getOwnPropertyDescriptor(objectBase, 'toString');
-console.log(descriptor);
+
+Object.defineProperty(person, 'name', {
+    writable: false,
+    enumerable: true,
+    configurable: false
+});
+
+delete person.name
+
+console.log(person);
